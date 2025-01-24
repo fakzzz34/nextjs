c4630c6e427731001a431891593e748efcf18d70
Fauzan Khan <fauzankhan@Fauzans-MacBook-Pro-14.local>
2025-01-24 22:55:27 +0800

    feat: create startup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Work_Sans } from "next/font/google";
 import "./globals.css";
 import "easymde/dist/easymde.min.css";
+import { Toaster } from "@/components/ui/toaster";
 
 const workSans = Work_Sans({
     variable: "--font-work-sans",
@@ -20,7 +21,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={workSans.variable}>{children}</body>
+            <body className={workSans.variable}>
+                {children}
+                <Toaster />
+            </body>
         </html>
     );
 }
