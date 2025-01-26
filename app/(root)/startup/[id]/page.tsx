d5a22a962b341f77c2a8dafcd6a91549019d3986
Fauzan Khan <fauzankhan@Fauzans-MacBook-Pro-14.local>
2025-01-26 18:41:52 +0800

    feat: profile page

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,6 +1,9 @@
 import { formatDate } from "@/lib/utils";
 import { client } from "@/sanity/lib/client";
-import { STARTUPS_BY_ID_QUERY } from "@/sanity/lib/queries";
+import {
+    PLAYLIST_BY_SLUG_QUERY,
+    STARTUPS_BY_ID_QUERY,
+} from "@/sanity/lib/queries";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
@@ -9,6 +12,7 @@ import React, { Suspense } from "react";
 import markdownit from "markdown-it";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
 const md = markdownit();
 
@@ -17,7 +21,12 @@ export const experimental_ppr = true;
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
     const id = (await params).id;
 
-    const post = await client.fetch(STARTUPS_BY_ID_QUERY, { id });
+    const [post, { select: editorPost }] = await Promise.all([
+        await client.fetch(STARTUPS_BY_ID_QUERY, { id }),
+        await client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+            slug: "editor-picks",
+        }),
+    ]);
 
     if (!post) return notFound();
 
@@ -74,6 +83,20 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
                 <hr className="divider" />
 
                 {/* TODO: EDITOR SELECTOR STARTUPS */}
+
+                {editorPost.length > 0 && (
+                    <div className="max-w-4xl mx-auto">
+                        <p className="text-30-semibold">Editor Picks</p>
+
+                        <ul className="mt-7 card_grid-sm">
+                            {editorPost.map(
+                                (post: StartupTypeCard, i: number) => (
+                                    <StartupCard key={i} post={post} />
+                                )
+                            )}
+                        </ul>
+                    </div>
+                )}
 
                 <Suspense fallback={<Skeleton className="view_skeleton" />}>
                     <View id={id} />
--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,9 +1,10 @@
-import { formatDate } from "@/lib/utils";
+import { cn, formatDate } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Author, Startup } from "@/sanity/types";
 import { EyeIcon } from "lucide-react";
+import { Skeleton } from "./ui/skeleton";
 
 export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
@@ -43,8 +44,8 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
                 </div>
                 <Link href={`/user/${author?._id}`}>
                     <Image
-                        src="https://placehold.co/48x48"
-                        alt="placeholder"
+                        src={author?.image!}
+                        alt={author?.name!}
                         width={48}
                         height={48}
                         className="rounded-full"
@@ -71,5 +72,15 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
         </li>
     );
 };
+
+export const StartupSkeleton = () => (
+    <>
+        {[0, 1, 2, 3, 4].map((index: number) => (
+            <li key={cn("skeleton", index)}>
+                <Skeleton className="startup-cart_skeleton" />
+            </li>
+        ))}
+    </>
+);
 
 export default StartupCard;
